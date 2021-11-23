8335026f190ed6477a77c40c6f6e67c30ab6c8f3
Juan Zitelli <juanzitelli7@gmail.com>
2021-11-22 22:33:29 -0300

    update main movies page

--- pages/movies/index.tsx
+++ pages/movies/index.tsx
@@ -1,9 +1,23 @@
+import { Button, Divider, Stack, Typography } from "@mui/material";
 import React from "react";
+import { MovieList } from "../../components/modules/movies/MovieList";
+import NextLink from "next/link";
 
-interface Props {}
-
-const MoviesPage = (props: Props) => {
-  return <div>MoviesPage</div>;
+const MoviesPage = () => {
+  return (
+    <>
+      <Stack direction="column" gap={2}>
+        <Typography variant="h2" sx={{ fontWeight: 700 }}>
+          Movies 🎬🍿
+        </Typography>
+        <Divider />
+        <NextLink href="/movies/new">
+          <Button variant="contained">Create new movie</Button>
+        </NextLink>
+        <MovieList />
+      </Stack>
+    </>
+  );
 };
 
 export default MoviesPage;
